78a59124d12f377f8717986e5ca39b23d070f008
Tony Walmsley <tony@aosd.co.uk>
2019-04-13 09:50:46 +0100

    Class comments.

--- lib/modl/parser/global_parse_context.rb
+++ lib/modl/parser/global_parse_context.rb
@@ -1,21 +1,22 @@
-module Modl::Parser
-  class GlobalParseContext
+module Modl
+  module Parser
+    # Each time we run the parser on a MODL file we need to keep track of a few things so they can be made available
+    # to other areas of the code. This class is the container for that contextual information, which is gathered
+    # as the Modl::Parser:Parsed object processes the parse tree.
+    class GlobalParseContext
+      attr_accessor :index
+      attr_accessor :pairs
+      attr_accessor :classes
+      attr_accessor :methods_hash
+      attr_accessor :conditional
 
-    attr_accessor :index
-    attr_accessor :pairs
-    attr_accessor :classes
-    attr_accessor :methods_hash
-    attr_accessor :conditional
-
-    def initialize
-      @index = []
-      @pairs = {}
-      @classes = {}
-      @methods_hash = {}
-      @conditional = 0
-    end
-
-    def process_classes obj
+      def initialize
+        @index = []
+        @pairs = {}
+        @classes = {}
+        @methods_hash = {}
+        @conditional = 0
+      end
     end
   end
 end
