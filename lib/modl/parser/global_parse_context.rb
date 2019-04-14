581d7b07a5ad9060fbbdc91d51519425864e9e9e
Tony Walmsley <tony@aosd.co.uk>
2019-04-14 09:00:45 +0100

    Comments.

--- lib/modl/parser/global_parse_context.rb
+++ lib/modl/parser/global_parse_context.rb
@@ -4,10 +4,15 @@ module Modl
     # to other areas of the code. This class is the container for that contextual information, which is gathered
     # as the Modl::Parser:Parsed object processes the parse tree.
     class GlobalParseContext
+      # Holds the index array from a MODL file, e.g. '?=a:b:c:d'
       attr_accessor :index
+      # Contains all pairs as they are encountered in the parsing process.
       attr_accessor :pairs
+      # Contains all defined and loaded classes for the current MODL document.
       attr_accessor :classes
+      # Hold the user-defined methods.
       attr_accessor :methods_hash
+      # Tracks the nesting depth for conditional clauses.
       attr_accessor :conditional
 
       def initialize
