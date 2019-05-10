28c0f12bbf885501deff97199cc584f4c4519d80
Tony Walmsley <tony@aosd.co.uk>
2019-05-10 11:00:31 +0100

    Ad-hoc test changed.

--- spec/modl/modl4.rb
+++ spec/modl/modl4.rb
@@ -3,25 +3,12 @@
 require 'modl'
 
 a = <<~XXXXXXXX
-  
-    _print_two=false;
-    (
-        one=1;
-        {
-            print_two?
-            two=2
-        };
-        three=3
-    );
-  
-  _c=us;
-  (
-    test=1;
-    {
-      c=gb?
-        country=UK
-    }
-  )
+
+  a=b
+  c
+  d
+  e
+
 XXXXXXXX
 
 result = MODL.parse(a)
